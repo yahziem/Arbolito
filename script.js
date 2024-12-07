a20e94de6a1eff35cbb07a432622c5fff019d3ce
yahziem <yahziga@hotmail.com>
2024-12-07 00:05:22 -0600

    .add gogle any

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ setInterval(() => {
     light.style.opacity = Math.random() > 0.5 ? '1' : '0.7';
   });
 }, 500);
+  // Comprobar si ya se ha dado el consentimiento
+  window.onload = function() {
+    if (!localStorage.getItem('cookies-accepted')) {
+      document.getElementById('cookie-banner').style.display = 'block';
+    } else {
+      document.getElementById('cookie-banner').style.display = 'none';
+    }
+  };
+
+  // Función para aceptar las cookies
+  document.getElementById('accept-cookies').addEventListener('click', function() {
+    localStorage.setItem('cookies-accepted', 'true'); // Guardar la preferencia
+    document.getElementById('cookie-banner').style.display = 'none'; // Ocultar el banner
+    // Aquí se puede cargar Google Analytics si es aceptado
+    gtag('config', 'G-V3TYXL31NM');
+  });
+
+  // Función para rechazar las cookies
+  document.getElementById('decline-cookies').addEventListener('click', function() {
+    localStorage.setItem('cookies-accepted', 'false'); // Guardar la preferencia
+    document.getElementById('cookie-banner').style.display = 'none'; // Ocultar el banner
+    // Aquí puedes bloquear la carga de Google Analytics si se rechaza
+  });
